da29ed99286d617c6b92ba4d854a9688626c2dcd
Nathan Allen <nallenscott@me.com>
2025-11-09 22:30:31 -0500

    refactor: move implementations to separate repos

--- cli/src/ocp_cli/__init__.py
+++
@@ -1,2 +0,0 @@
-"""OCP CLI package"""
-__version__ = "0.2.0"
--- registry/src/ocp_registry/__init__.py
+++
@@ -1,9 +0,0 @@
-"""OCP Registry package initialization."""
-
-from .main import app
-from .models import *
-from .service import RegistryService
-from .validator import APIValidator
-
-__version__ = "0.1.0"
-__all__ = ["app", "RegistryService", "APIValidator"]
