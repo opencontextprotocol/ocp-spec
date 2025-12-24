21026977c83d36e3dbdcdcb7367f69fbe394a8cf
Nathan Allen <nallenscott@me.com>
2025-12-24 05:54:57 -0500

    feat: add Tailwind CSS configuration and styles

---
+++ docs/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'ocp:',
+  content: [
+    './content/**/*.md',
+    './layouts/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
