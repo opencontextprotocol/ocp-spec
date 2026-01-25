af5842505ed3821bee97b230bdd99b2d6393ad8f
Nathan Allen <nallenscott@me.com>
2026-01-25 08:13:09 -0500

    feat: enhance badge system and typography styles

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './content/**/*.md',
-    './layouts/**/*.html',
+    './**/*.md',
+    './**/*.html',
+    '../**/*.j2',
   ],
   theme: {
     extend: {
